9fb23f1a96c6e95a47208d29f94c9a4b081c2616
Giovanni Charles <giovanni.charles@gmail.com>
2015-09-12 16:53:29 +0100

    item endpoint and user endpoint extended

---
+++ server/api/item/item.model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+  name: String,
+  img: String,
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
